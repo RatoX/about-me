84e02fa2c807f9a1915bd4ef5ccb5da057942c2b
Rodrigo Alencar <rodrigovieira18@gmail.com>
2016-11-06 01:43:59 -0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path              = require('path');
+    webpack           = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+var config = {
+  entry: {
+    script: './scripts/index.js',
+    style: './styles/index.scss',
+    vendor: ['mithril']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)\//,
+          loader: 'babel-loader'
+      },
+      {
+        test: /\.scss/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+      },
+      {
+        test: /\.(jpg)$/,
+        loader: 'file?name=images/[name].[ext]'
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({ m: 'mithril' }),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' }),
+    new ExtractTextPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      template: 'templates/index.html.ejs'
+    })
+  ],
+
+  resolve: {
+    root: path.join(__dirname, 'scripts'),
+    extensions: ['', '.js'],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+};
+
+module.exports = config;
