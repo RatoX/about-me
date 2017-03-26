b2b7dfc5f56d4fe37d85185582472c5d78f6ef59
Rodrigo Alencar <rodrigovieira18@gmail.com>
2017-03-25 22:57:58 -0300

    Updates

--- script.js
+++ script.js
@@ -1,1 +1,286 @@
-!function(t){function e(o){if(n[o])return n[o].exports;var r=n[o]={exports:{},id:o,loaded:!1};return t[o].call(r.exports,r,r.exports,e),r.loaded=!0,r.exports}var n={};return e.m=t,e.c=n,e.p="",e(0)}([function(t,e,n){"use strict";function o(t){return t&&t.__esModule?t:{default:t}}var r=n(1),a=(o(r),n(3));o(a)},function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var o=n(2),r={translations:{},locale:"pt-BR",t:function(t){var e="not found",n=this.translations[this.locale];return n&&(e=(0,o.resolveObjectByPath)(n,t)),e}},a={myself:{about:"I am a FullStack developer with focus on FrontEnd.",contact:{fingerprint:{pgp:"Do you want to talk to me using cryptography?"}}}},u={myself:{about:"Desenvolvedor FullStack com foco em FrontEnd.",contact:{fingerprint:{pgp:"Quer me entrar em contato comigo de forma segura?"}}}};r.translations.en=a,r.translations["pt-BR"]=u,e.default=r},function(t,e){"use strict";function n(){var t="pt-BR";return window.location.hash&&(t=window.location.hash.substring(1)),"en"===t?"en":"pt-BR"}function o(){var t=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"";return t.replace(/^\.|\#|\s*/g,"")}function r(t){return document.querySelector(t)}function a(t){return document.querySelectorAll(t)}function u(t,e){return e.split(".").reduce(function(t,e){return t?t[e]:void 0},t||self)}Object.defineProperty(e,"__esModule",{value:!0}),e.getLanguageFromHash=n,e.toKey=o,e.$=r,e.$$=a,e.resolveObjectByPath=u},function(t,e,n){"use strict";function o(t){return t&&t.__esModule?t:{default:t}}var r=n(1),a=o(r),u=n(2),c=[".myself .about",".myself .contact .fingerprint .pgp"],l=function(){var t=arguments.length>0&&void 0!==arguments[0]?arguments[0]:(0,u.getLanguageFromHash)();a.default.locale=t,c.forEach(function(t){var e=(0,u.$)(t),n=(0,u.toKey)(t);e.innerHTML=a.default.t(n)})},i=function(t,e){var n=(0,u.$$)(t);Array.prototype.forEach.call(n,function(t){t.classList.remove(e)})},s=(0,u.$$)(".myself a.language");Array.prototype.forEach.call(s,function(t){t.onclick=function(t){t.preventDefault(),i(".myself a.language","active");var e=t.target,n=e.hash.substring(1);e.classList.toggle("active"),l(n)}}),l()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _translations = __webpack_require__(1);
+
+	var _translations2 = _interopRequireDefault(_translations);
+
+	var _myself = __webpack_require__(3);
+
+	var _myself2 = _interopRequireDefault(_myself);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _util = __webpack_require__(2);
+
+	var I18n = {
+	  translations: {},
+	  locale: 'pt-BR',
+	  t: function translate(key) {
+	    var result = "not found";
+	    var translation = this.translations[this.locale];
+	    if (translation) {
+	      result = (0, _util.resolveObjectByPath)(translation, key);
+	    }
+
+	    return result;
+	  }
+	};
+
+	var en = {
+	  myself: {
+	    about: "I am a FullStack developer.",
+	    contact: {
+	      fingerprint: {
+	        pgp: "Do you want to talk to me using cryptography?"
+	      }
+	    },
+	    profile: {
+	      back: "< Back"
+	    },
+	    'know-more': {
+	      action: "See my skills >>"
+	    }
+	  }
+	};
+
+	var ptBR = {
+	  myself: {
+	    about: "Desenvolvedor FullStack.",
+	    contact: {
+	      fingerprint: {
+	        pgp: "Quer me entrar em contato comigo de forma segura?"
+	      }
+	    },
+	    profile: {
+	      back: "<< Voltar"
+	    },
+	    'know-more': {
+	      action: "Minhas habilidades >>"
+	    }
+	  }
+	};
+
+	I18n.translations["en"] = en;
+	I18n.translations["pt-BR"] = ptBR;
+
+	exports.default = I18n;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.getLanguageFromHash = getLanguageFromHash;
+	exports.toKey = toKey;
+	exports.$ = $;
+	exports.$$ = $$;
+	exports.resolveObjectByPath = resolveObjectByPath;
+	exports.removeClass = removeClass;
+	function getLanguageFromHash() {
+	  var language = "pt-BR";
+	  if (window.location.hash) {
+	    language = window.location.hash.substring(1);
+	  }
+
+	  if (language === "en") {
+	    return "en";
+	  }
+	  return "pt-BR";
+	}
+
+	function toKey() {
+	  var query = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+
+	  return query.replace(/^\.|\#|\s*/g, '');
+	}
+
+	function $(el) {
+	  return document.querySelector(el);
+	}
+
+	function $$(el) {
+	  return document.querySelectorAll(el);
+	}
+
+	function resolveObjectByPath(obj, path) {
+	  return path.split('.').reduce(function (prev, curr) {
+	    return prev ? prev[curr] : undefined;
+	  }, obj || self);
+	}
+
+	function removeClass(query, cssClass) {
+	  var elements = $$(query);
+	  Array.prototype.forEach.call(elements, function (element) {
+	    element.classList.remove(cssClass);
+	  });
+	}
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var _translations = __webpack_require__(1);
+
+	var _translations2 = _interopRequireDefault(_translations);
+
+	var _knowMore = __webpack_require__(4);
+
+	var _knowMore2 = _interopRequireDefault(_knowMore);
+
+	var _util = __webpack_require__(2);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var stringsToTranslate = ['.myself .about', '.myself .contact .fingerprint .pgp', '.myself .know-more .action', '.myself .profile .back'];
+
+	var translate = function translate() {
+	  var language = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : (0, _util.getLanguageFromHash)();
+
+	  _translations2.default.locale = language;
+
+	  stringsToTranslate.forEach(function (queryElement) {
+	    var element = (0, _util.$)(queryElement);
+
+	    if (element) {
+	      var keyTranslation = (0, _util.toKey)(queryElement);
+	      element.innerHTML = _translations2.default.t(keyTranslation);
+	    }
+	  });
+	};
+
+	var initTranslate = function initTranslate() {
+	  Array.prototype.forEach.call((0, _util.$$)(".myself a.language"), function (element) {
+	    element.addEventListener('click', function (event) {
+	      event.preventDefault();
+	      (0, _util.removeClass)(".myself a.language", "active");
+
+	      var element = event.target;
+	      var language = element.hash.substring(1);
+	      element.classList.toggle("active");
+	      translate(language);
+	    });
+	  });
+
+	  translate();
+	};
+
+	document.addEventListener('DOMContentLoaded', function () {
+	  initTranslate();
+	  _knowMore2.default.init('.know-more');
+	});
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _translations = __webpack_require__(1);
+
+	var _translations2 = _interopRequireDefault(_translations);
+
+	var _util = __webpack_require__(2);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function KnowMore() {}
+
+	KnowMore.prototype.createAction = function () {
+	  var button = document.createElement('span');
+	  var buttonText = document.createTextNode(_translations2.default.t('myself.know-more.action'));
+
+	  button.classList.add('action');
+	  button.appendChild(buttonText);
+	  return button;
+	};
+
+	KnowMore.prototype.init = function () {
+	  var mainClass = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+
+	  var knowMoreSection = (0, _util.$)("" + mainClass);
+	  var button = this.createAction();
+	  var sectionTwo = (0, _util.$)('.profile .two');
+
+	  button.addEventListener('click', function () {
+	    sectionTwo.classList.remove('hidden');
+	    sectionTwo.classList.add('fade-in');
+	  });
+
+	  sectionTwo.querySelector('.action.back').addEventListener('click', function () {
+	    sectionTwo.classList.remove('fade-in');
+	    sectionTwo.classList.add('hidden');
+	  });
+
+	  knowMoreSection.appendChild(button);
+	};
+
+	exports.default = Object.create(KnowMore.prototype);
+
+/***/ }
+/******/ ]);
